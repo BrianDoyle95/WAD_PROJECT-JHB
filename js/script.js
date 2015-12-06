95480c71be34748ea7d3aeb1815f2aa472931f72
brian doyle <brian doyle>
2015-12-06 15:55:06 +0000

    a

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function calendar(){
+    var day=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var month=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var date=new Date();
+    setText('calendar-day', day[d.getDay()]);
+    setText('calendar-date', date.getDate());
+    setText('calendar-month-year', month[d.getMonth()] +' '+(1900+date.getYear()));
+};
+
+function setText(id, val){
+    if(val < 10){
+        val = '0' + val;  // Add leading 0 if val < 10
+    }
+    document.getElementById(id).innerHTML = val;
+};
+
+
+window.onload = calendar;
